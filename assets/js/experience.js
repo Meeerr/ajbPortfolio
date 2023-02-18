22e97c1942d27394aed34771fcba4175b329a433
Almer June N. Ba-ad <120560008+Meeerr@users.noreply.github.com>
2023-02-18 21:25:32 +0800

    Add files via upload

---
+++ assets/js/experience.js
@@ -0,0 +1,45 @@
+AOS.init();
+
+//  Work experience cards
+
+const experiencecards = document.querySelector(".experience-cards");
+const exp = [
+  {
+    title: "I don't have any work experience",
+    cardImage: "",
+    place: "",
+    time: "(Present)",
+    desp: "",
+  },
+  
+];
+
+const showCards2 = () => {
+  let output = "";
+  exp.forEach(
+    ({ title, cardImage, place, time, desp }) =>
+      (output += `        
+    <div class="col gaap" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="400"> 
+      <div class="card card1">
+        <img src="${cardImage}" class="featured-image"/>
+        <article class="card-body">
+          <header>
+            <div class="title">
+              <h3>${title}</h3>
+            </div>
+            <p class="meta">
+              <span class="pre-heading">${place}</span><br>
+              <span class="author">${time}</span>
+            </p>
+            <ol>
+              ${desp}
+            </ol>
+          </header>
+        </article>
+      </div>
+    </div>
+      `)
+  );
+  experiencecards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards2);
